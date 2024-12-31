a9f7f847dcb9c24a8c953055ab87c93e56318183
DESKTOP-9MEJLQT\BOGURA <s.m.tariqul.islam.eu181400082@gmail.com>
2024-12-31 12:49:41 +0600

    inital ok author frontend

--- author/views.py
+++ author/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from . import forms
+
 # Create your views here.
+
+def add_author(request):
+    author_form=forms.AuthorForm()
+    return render(request,'add_author.html', {'form':author_form})
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -41,7 +41,12 @@ INSTALLED_APPS = [
     'categories',
     'posts',
     'profiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,7 +63,7 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
